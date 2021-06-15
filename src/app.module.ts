65d40e5b9d648b6766c12f1569229df602a29daf
Eri11 <erikaeli11@gmail.com>
2021-06-14 22:07:48 -0600

    creacion de 3er controlador - paciente

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MedicoController } from './medico/medico.controller';
 import { EspecialidadController } from './especialidad/especialidad.controller';
+import { PacienteController } from './paciente/paciente.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, MedicoController, EspecialidadController],
+  controllers: [AppController, MedicoController, EspecialidadController, PacienteController],
   providers: [AppService],
 })
 export class AppModule {}
