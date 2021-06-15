3724067d07248b9609fc5b01ba75f3a32265cedd
Eri11 <erikaeli11@gmail.com>
2021-06-14 20:51:17 -0600

    1er mod - creacion controlador medico

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MedicoController } from './medico/medico.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, MedicoController],
   providers: [AppService],
 })
 export class AppModule {}
